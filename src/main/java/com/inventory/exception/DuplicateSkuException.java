46fc546dd1343157bbde74b0a495f63745f5266e
Edward <pakachun48@gmail.com>
2026-06-25 02:28:04 +0300

    feat: add DuplicateSkuException

---
+++ src/main/java/com/inventory/exception/DuplicateSkuException.java
@@ -0,0 +1,7 @@
+package com.inventory.exception;
+
+public class DuplicateSkuException extends RuntimeException {
+    public DuplicateSkuException(String sku) {
+        super("Item with SKU '" + sku + "' already exists");
+    }
+}
